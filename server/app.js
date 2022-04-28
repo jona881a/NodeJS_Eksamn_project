ae799103c78892032c4ab0ad62e289b22c6b0b68
jona881a <jonaskunert@hotmail.com>
2022-04-28 08:57:16 +0200

    added necessities to make the server run

--- server/app.js
+++ server/app.js
@@ -1,5 +1,10 @@
 import express from "express";
 import authRouter from "./routers/auth.js";
+import http from "http";
 
 const app = express();
+const server = http.createServer(app);
+
 app.use(authRouter);
+
+server.listen(3000);
