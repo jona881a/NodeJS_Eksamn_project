59e209da76bfb2893ba4750c01725c09d33924de
oliverbskau <oliverbskau@gmail.com>
2022-05-03 12:20:11 +0200

    installed CORS to package.json, and used it in app.js

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import authRouter from "./routers/auth.js";
 import mailRouter from "./routers/nodemailer.js";
+import cors from "cors";
 
 
 import http from "http";
@@ -8,6 +9,8 @@ import http from "http";
 const app = express();
 const server = http.createServer(app);
 
+app.use(cors());
+app.use(express.json());
 app.use(authRouter);
 app.use(mailRouter);
 
