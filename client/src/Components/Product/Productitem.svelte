258d7dcee3084d53bb77453e52795e9c03f0bea4
jona881a <jonaskunert@hotmail.com>
2022-05-09 10:50:13 +0200

    created a selectedgame attr in store

--- client/src/Components/Product/Productitem.svelte
+++ client/src/Components/Product/Productitem.svelte
@@ -1,12 +1,18 @@
 <script>
+  import { selectedGame } from "../../stores/stores.js";
     export let product;
+
+    function handleBuyGame() {
+      selectedGame.set(product);
+      console.log($selectedGame);
+    }
 
 </script>
 
 <div class="card">
   <div class="card-img">
     <!-- svelte-ignore a11y-missing-attribute -->
-    <img src={product.img1} width="300" height="500"/>
+    <img src={product.img1} width="200" height="200"/>
   </div>
   <div class="card-body">
     <div class="game-title">
@@ -15,7 +21,7 @@
     <div class="game-genre">
       <p>{product.genre}</p>
     </div>
-    <button class="card-purchase-btn">
+    <button class="card-purchase-btn" on:click={handleBuyGame}>
       Buy 
       <i class="fa-solid fa-basket-shopping"></i>
     </button>
--- client/src/stores/stores.js
+++ client/src/stores/stores.js
@@ -3,3 +3,5 @@ import { writable, readable } from "svelte/store";
 export const user = writable(null);
 
 export const session = writable(null);
+
+export const selectedGame = writable(null);
